6969ed6c8d4cbc9d549df56cbdfb6c49c4d6c698
messiasGeovani <messias.geovani.lima@gmail.com>
2023-03-19 20:51:51 -0300

    remove unused authentication guard params

--- src/app/authentication/guards/authentication.guard.ts
+++ src/app/authentication/guards/authentication.guard.ts
@@ -1,25 +1,31 @@
 import { Injectable, Injector } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationGuard implements CanActivate {
   constructor(private injector: Injector) {}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const authenticationService = this.injector.get(AuthenticationService);
-      const isLogged = authenticationService.isLogged();
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const authenticationService = this.injector.get(AuthenticationService);
+    const isLogged = authenticationService.isLogged();
 
-      if (isLogged) {
-        return true;
-      }
+    if (isLogged) {
+      return true;
+    }
 
-      authenticationService.logout();
-      return false;
+    authenticationService.logout();
+    return false;
   }
-  
 }
